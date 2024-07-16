6370d30fb30914f4ecee42f3be98c5be5a77cc50
DiegoCoura <diegogomes.10.9@gmail.com>
2024-07-16 20:02:54 -0300

    feat: category model now has a virtual to get the url

--- models/category.js
+++ models/category.js
@@ -5,4 +5,8 @@ const CategorySchema = new Schema({
     name: { type: String, required: true }
 })
 
+CategorySchema.virtual("url").get(function(){
+    return `/catalog/categories/${this._id}`
+})
+
 module.exports = mongoose.model("Category", CategorySchema)
