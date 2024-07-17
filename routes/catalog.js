91b5cc4cf7611b1befa70f117f1db42fe86feb92
DiegoCoura <diegogomes.10.9@gmail.com>
2024-07-17 00:14:34 -0300

    feat: routes for category CRUD operations

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,27 +1,33 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-const catalogController = require('../controllers/catalogController')
+const catalogController = require("../controllers/catalogController");
 
+router.get("/", catalogController.index);
 
-router.get('/', catalogController.index);
+router.get("/createcategory", catalogController.category_create_get);
 
-router.get('/createproduct', catalogController.product_create_get);
+router.post("/createcategory", catalogController.category_create_post);
 
-router.post('/createproduct', catalogController.product_create_post);
+router.get("/category/:id/delete", catalogController.category_delete_get);
 
-router.get('/createcategory', catalogController.category_create_get);
+router.post("/category/:id/delete", catalogController.category_delete_post);
 
-router.post('/createcategory', catalogController.category_create_post);
+router.get("/categories", catalogController.categories_get);
 
-router.get('/product/:id', catalogController.product_detail);
+router.get("/category/:id", catalogController.category_get_products);
 
-router.get('/product/:id/update', catalogController.product_update_get);
+router.get("/createproduct", catalogController.product_create_get);
 
-router.post('/product/:id/update', catalogController.product_update_post);
+router.post("/createproduct", catalogController.product_create_post);
 
-router.get('/product/:id/delete', catalogController.product_delete_get)
+router.get("/product/:id", catalogController.product_detail);
 
-router.post('/product/:id/delete', catalogController.product_delete_post)
+router.get("/product/:id/update", catalogController.product_update_get);
 
+router.post("/product/:id/update", catalogController.product_update_post);
+
+router.get("/product/:id/delete", catalogController.product_delete_get);
+
+router.post("/product/:id/delete", catalogController.product_delete_post);
 
 module.exports = router;
